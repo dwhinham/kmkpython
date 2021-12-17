b44429254919d7ffe2513e0530455d229ed0d600
Dale Whinham <daleyo@gmail.com>
2021-12-17 09:43:03 +0000

    Pico: Disable MIDI/mouse, freeze kmk and dependencies

--- ports/raspberrypi/boards/raspberry_pi_pico/mpconfigboard.mk
+++ ports/raspberrypi/boards/raspberry_pi_pico/mpconfigboard.mk
@@ -9,3 +9,15 @@ CHIP_FAMILY = rp2
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
 
 CIRCUITPY__EVE = 1
+
+CIRCUITPY_USB_MIDI = 0
+CIRCUITPY_USB_HID_MOUSE = 0
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BLE
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SSD1306
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BLE
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/kmk
